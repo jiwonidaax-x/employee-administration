b38d30a91e12db3b6458dda5a3f4c16f0b2ac94f
jiwonidaax-x <jgb07012@gmail.com>
2022-09-16 00:18:22 -0400

    server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var HTTP_PROT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+var path = require("path");
+
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PROT);
+}
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
-app.listen(HTTP_PROT);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+// setup the static folder that static resources can load from
+// like images, css files, etc.
+app.use(express.static('public'));
+
+app.listen(HTTP_PROT, onHttpStart);
