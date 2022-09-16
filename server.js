e4d1943d89af9b1ab2cb197f22f212d0376497e5
jiwonidaax-x <jgb07012@gmail.com>
2022-09-15 23:47:21 -0400

    html

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var HTTP_PROT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
-app.get("/",(req,res)=>{
-res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(HTTP_PROT);
