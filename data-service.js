bf5df1903a948b2c4363f8cbb0e944017ea63909
jiwonidaax-x <jgb07012@gmail.com>
2022-09-16 13:14:14 -0400

    getDepartments

--- data-service.js
+++ data-service.js
@@ -1,1 +1,76 @@
-//data-service.js
+var file = require("fs");
+const { resolve } = require("path");
+var employees = [];
+var departments = [];
+
+initialize = () => {
+  return new Promise = ((resolve, reject) => {
+    file.readFile('./data/employees.json', (err, data) => {
+      if (err) {
+        reject("unable to read file");
+      }
+      else {
+        employees = JSON.parse(data);
+      }
+    });
+
+    file.readFile('./data/departments.json', (err, data) => {
+      if (err) {
+        reject("unable to read file");
+      } else {
+        departments = JSON.parse(data);
+      }
+    })
+    resolve();
+  })
+};
+
+getAllEmployees = () => {
+  return new Promise = ((resolve, reject) => {
+    if (employees.length == 0) {
+      reject("no results returned");
+    }
+    else {
+      resolve(employees);
+    }
+  })
+};
+
+getManagers = () => {
+  var newArr = [];
+  return new Promise = ((resolve, reject) => {
+    if (employees.length == 0) {
+      reject("no results returned");
+    }
+    else {
+      employees.forEach(e => newArr.push(e.isManager == true));
+      resolve(newArr);
+    }
+  })
+};
+
+
+getDepartments = () => {
+  return new Promise = ((resolve, reject => {
+
+
+  })
+ };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
