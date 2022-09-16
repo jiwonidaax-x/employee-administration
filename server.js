49af13a939433b6eb910e35a6199b558f9fced6c
jiwonidaax-x <jgb07012@gmail.com>
2022-09-16 00:46:55 -0400

    dataservice

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var HTTP_PROT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
+var dataservice = require(__dirname + "/data-service.js");
 
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PROT);
